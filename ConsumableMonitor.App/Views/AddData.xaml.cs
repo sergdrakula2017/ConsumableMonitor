43579daa885080657674ffbe88ce5d1c3966b430
serg201510 <serg201510@yandex.ru>
2022-04-01 17:58:27 +0300

    создааны модели клиента

---
+++ ConsumableMonitor.App/Views/AddData.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace ConsumableMonitor.App.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для AddData.xaml
+    /// </summary>
+    public partial class AddData : Window
+    {
+        public AddData()
+        {
+            InitializeComponent();
+        }
+
+       
+
+    }
+}
